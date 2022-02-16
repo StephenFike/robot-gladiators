21bfd31f5f8ba4a5bc38afc020479ba28438dfab
Stephen Fike <sfike98@gmail.com>
2022-02-15 18:58:28 -0600

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,21 +1,33 @@
+// Game States
+// "WIN" - Player robot has defeated all enemy-robots
+    // Fight all enemy-robots
+    // Defeat each enemy-robot
+// "LOSE" - Player robot's health is zero or less
+
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-console.log(playerName, playerHealth, playerAttack);
 
-
-var enemyName = "Roborto";
+var enemyNames = ["Roberto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+
+for(var i = 0; i < enemyNames.length; i++) {
+    console.log(enemyNames[i]);
+    console.log(i);
+    console.log(enemyNames[i] + " is at " + i + " index");
+}
+
+
+var fight = function(enemyName) {
     window.alert("Welcome to Robot Gladiators!");
 
 var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.")
 
-if (promptFight === "fight" || promptFight === "FIGHT") {
+if (promptFight === "fight" || promptFight === "FIGHT" || promptFight === "Fight") {
     enemyHealth = enemyHealth - playerAttack;
     console.log(
         playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
@@ -28,14 +40,14 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
 
     playerHealth = playerHealth - enemyAttack;
     console.log(
-        enemyName + " attacked" + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
     );
     if (playerHealth <= 0) {
         window.alert(playerName + " has died!")
     } else {
         window.alert(playerName + " still has " + playerHealth + " health left.");
     }
-} else if (promptFight === "skip" || promptFight === "SKIP") {
+} else if (promptFight === "skip" || promptFight === "SKIP" || promptFight === "Skip") {
     var confirmSkip = window.confirm("Are you sure you'd like to quit?");
 
     if (confirmSkip) {
@@ -50,4 +62,6 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
 
 };
 
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
